c5fcb6609e97edde0f53754f25a02317ef0ef77a
lxsca00 <ljimena10@gmail.com>
2022-10-19 11:15:52 -0500

    complete deleteItem

--- burger-queen/src/pages/Waiter.jsx
+++ burger-queen/src/pages/Waiter.jsx
@@ -18,15 +18,11 @@ export default function Waiter() {
   }, []);
 
   const filteredProducts = products.filter((product) => product.type === type);
-  
-  const uniqueProduct = (id) => {
-      const unique = order.find((obj) => obj.id === id);
-      return unique;
-    };
+
+  const repeatedProduct = (id) => order.find((obj) => obj.id === id);
 
   const addProducts = (product) => {
-
-    if (uniqueProduct(product.id)) {
+    if (repeatedProduct(product.id)) {
       const addQtyPrice = order.map((item) => {
         if (item.id === product.id) {
           const newOrder = item;
@@ -40,35 +36,34 @@ export default function Waiter() {
     //console.log(order, "arrayorder");
   };
 
-  const originalPrice = (id) => products.find( product => product.id === id).price
+  const originalPrice = (id) =>
+    products.find((product) => product.id === id).price;
 
   const deleteItem = (product) => {
-    if(uniqueProduct(product.id)){
-      const deleteProd= order.map(obj => {
-        if(obj.id === product.id){
-          const item = obj;
-          if(item.quantity > 1) {
-            item.quantity--;
-            item.price = originalPrice(product.id) * item.quantity
-          } if (item.quantity === 1) {
-            order.filter(elem => elem.id !==item.id)
-          }
-        } 
-        return obj
-      })
-      setOrder(deleteProd)
-    } else setOrder([...order, { ...product, quantity: 1 }]);
-   };
+    const deletedProduct = order.reduce((acc, elem) => {
+      if (elem.id === repeatedProduct(product.id).id) {
+        if (elem.quantity > 1) {
+          elem.quantity--;
+          elem.price = originalPrice(product.id) * elem.quantity;
+          acc.push(elem);
+        }
+      } else {
+        acc.push(elem);
+      }
+      return acc;
+    }, []);
+    setOrder(deletedProduct);
+  };
 
   let total = 0;
-  order.map((item) => {
+  order.forEach((item) => {
     total += item.price;
     return total;
   });
 
   return (
     <>
-      <Header img={mesero} view={'waiter'} nav={<NavWaiter/>}/>
+      <Header img={mesero} view={"waiter"} nav={<NavWaiter />} />
       <div className={style.container}>
         <ul className={style.buttonContainer}>
           <li className={style.buttonMenu} onClick={() => setType("Breakfast")}>
